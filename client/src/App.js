abaa08cb9c7ef34c06a8ec866bc31c2980e2cd5c
John Lu <john493@michaels.com>
2023-05-08 21:56:41 +0800

    fix error

--- client/src/App.js
+++ client/src/App.js
@@ -34,12 +34,16 @@ const App = () => {
     const conversationsContext = useIndexedDB(conversationsStore);
     const messagesContext = useIndexedDB(messagesStore, initialMsg);
     const {dbData: conversationsDbData , saveDataToDB: saveConversationsToDB} = conversationsContext;
+    const [storeConversationId, setStoreConversationId] = useLocalStorage(SelectedConversationIdKey, '');
     const [selectedConversationId, setSelectedConversationId] = useState('')
     const [selectedSystemPromote,setSelectedSystemPromote]=useState('')
 
     const [initLoadConversationsIsDone, setInitLoadConversationsIsDone] = useState(false);
     const [saveLoading, setSaveLoading] = useState(false);
 
+    useEffect(() => {
+        setSelectedConversationId(storeConversationId);
+    }, [storeConversationId])
 
     /**
      * The initialization waits for the data in indexedDB to be asynchronously fetched
@@ -47,7 +51,11 @@ const App = () => {
     useEffect(() => {
         setTimeout(() => {
             setInitLoadConversationsIsDone(true)
-
+            Array.from(conversationsDbData.values()).map((conversation) => {
+                if (conversation.id==storeConversationId){
+                    setSelectedSystemPromote(conversation.promote)
+                }
+                    })
         }, 300)
     }, [conversationsDbData.size])
 
